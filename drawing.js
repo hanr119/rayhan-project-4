69a9892fffa726c4cd9a03b5f3587a32e3160e1c
hanr119 <70984148+hanr119@users.noreply.github.com>
2021-05-04 09:30:10 -0400

    almost there

--- drawing.js
+++ drawing.js
@@ -55,8 +55,84 @@ document.addEventListener("keydown", function(event){
   if (event.key == "0"){
     ctx.clearRect(0, 0, canvas.width, canvas.height);   
   }  
-  if (event.key == "Enter"){
-    let letterTraced = document.getElementById("letterTraced");
-    letterTraced.innerHTML = "a"; 
+  if (event.key == "a"){
+    // let letterTraced = document.getElementById("letterTraced");
+    // letterTraced.innerHTML = "a"; 
+    canvas.style.backgroundImage = "url('../assets/a-lower.svg')";
   }  
+  if (event.key == "b"){
+    canvas.style.backgroundImage = "url('../assets/b-lower.svg')";
+  }
+  if (event.key == "c"){
+    canvas.style.backgroundImage = "url('../assets/c-lower.svg')";
+  }
+  if (event.key == "d"){
+    canvas.style.backgroundImage = "url('../assets/d-lower.svg')";
+  }
+  if (event.key == "e"){
+    canvas.style.backgroundImage = "url('../assets/e-lower.svg')";
+  }
+  if (event.key == "f"){
+    canvas.style.backgroundImage = "url('../assets/f-lower.svg')";
+  }
+  if (event.key == "g"){
+    canvas.style.backgroundImage = "url('../assets/g-lower.svg')";
+  }
+  if (event.key == "h"){
+    canvas.style.backgroundImage = "url('../assets/h-lower.svg')";
+  }
+  if (event.key == "i"){
+    canvas.style.backgroundImage = "url('../assets/i-lower.svg')";
+  }
+  if (event.key == "j"){
+    canvas.style.backgroundImage = "url('../assets/j-lower.svg')";
+  }
+  if (event.key == "k"){
+    canvas.style.backgroundImage = "url('../assets/k-lower.svg')";
+  }
+  if (event.key == "l"){
+    canvas.style.backgroundImage = "url('../assets/l-lower.svg')";
+  }
+  if (event.key == "m"){
+    canvas.style.backgroundImage = "url('../assets/m-lower.svg')";
+  }
+  if (event.key == "n"){
+    canvas.style.backgroundImage = "url('../assets/n-lower.svg')";
+  }
+  if (event.key == "o"){
+    canvas.style.backgroundImage = "url('../assets/o-lower.svg')";
+  }
+  if (event.key == "p"){
+    canvas.style.backgroundImage = "url('../assets/p-lower.svg')";
+  }
+  if (event.key == "q"){
+    canvas.style.backgroundImage = "url('../assets/q-lower.svg')";
+  }
+  if (event.key == "r"){
+    canvas.style.backgroundImage = "url('../assets/r-lower.svg')";
+  }
+  if (event.key == "s"){
+    canvas.style.backgroundImage = "url('../assets/s-lower.svg')";
+  }
+  if (event.key == "t"){
+    canvas.style.backgroundImage = "url('../assets/t-lower.svg')";
+  }
+  if (event.key == "u"){
+    canvas.style.backgroundImage = "url('../assets/u-lower.svg')";
+  }
+  if (event.key == "v"){
+    canvas.style.backgroundImage = "url('../assets/v-lower.svg')";
+  }
+  if (event.key == "w"){
+    canvas.style.backgroundImage = "url('../assets/w-lower.svg')";
+  }
+  if (event.key == "x"){
+    canvas.style.backgroundImage = "url('../assets/x-lower.svg')";
+  }
+  if (event.key == "y"){
+    canvas.style.backgroundImage = "url('../assets/y-lower.svg')";
+  }
+  if (event.key == "z"){
+    canvas.style.backgroundImage = "url('../assets/z-lower.svg')";
+  }
 })
